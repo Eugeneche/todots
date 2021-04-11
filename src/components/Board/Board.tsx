6249b4829ef368db402ffecbfbcbf4eee58d3adc
Eugeneche <eugene.bizmail@gmail.com>
2021-04-11 16:04:07 +0200

    Deployed to Amazon S3

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -79,13 +79,13 @@ export const Board = () => {
       <header className="board__header">
         <h1 className="board__title">Task manager</h1>
         {/* <button className="board__filter-btn"><img src={filter} alt="filter button"/></button> */}
-{        <label className="board__filter-select">
+        <label className="board__filter-select">
           Sort By:&nbsp;
           <select onChange={onChangeFilter}>
-            <option defaultValue="more_important_first">Later created first</option>
+            <option value="more_important_first">More important first</option>
             <option value="less_important_first">Less important first</option>
           </select>
-        </label>}
+        </label>
         <button className="board__add-btn" onClick={() => setMode(true)}><img src={add} alt="add task button"/></button>            
       </header> 
       <div className="board__tasks">       
